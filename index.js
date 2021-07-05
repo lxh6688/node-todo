477f70645846718c7c3cfc83ee20413692610797
Lxh6688 <1272662790@qq.com>
2021-07-05 23:03:06 +0800

    优化 add 功能

--- index.js
+++ index.js
@@ -3,35 +3,13 @@ const home = process.env.HOME || homedir
 const p = require('path')
 const fs = require('fs')
 const dbPath = p.join(home, '.todo')
+const db = require('./db.js')
 
-module.exports.add = (title) => {
+module.exports.add = async (title) => {
 //  读取之前的任务
+  const list = await db.read()
 //  往里面添加一个 title 任务
+  list.push({title, done:false})
 //  存储任务到文件
-
-
-  fs.readFile(dbPath, {flag: 'a+'},(error, data) => {
-    if(error){console.log(error)}
-    else{
-      let list
-      try {
-        list = JSON.parse(data.toString())
-      } catch (error2) {
-        list = []
-      }
-      console.log(list)
-      const task = {
-        title: title,
-        done: false
-      }
-      list.push(task)
-      const string = JSON.stringify(list)
-      fs.writeFile(dbPath, string+'\n', (error3) => {
-        if(error3){
-          console.log(error3)
-        }
-      })
-    }
-
-  })
+  db.write(list)
 }
