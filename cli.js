c95e7fa96471bc457e963d120642ab7f471ee858
Lxh6688 <1272662790@qq.com>
2021-07-06 23:27:54 +0800

    完成所有功能

--- cli.js
+++ cli.js
@@ -8,13 +8,17 @@ program
   .description('add a task')
   .action((...args) => {
     const words = args.slice(0,-1).join(' ')
-    api.add(words)
+    api.add(words).then(() => {console.log('添加成功')}, () => {console.log('添加失败')})
   });
 program
   .command('clear')
   .description('clear all task')
-  .action((...args) => {
-    console.log('this is clear')
+  .action(() => {
+    api.clear().then(() => {console.log('消除完毕')}, () => {console.log('消除失败')})
   });
 
 program.parse(process.argv);
+
+if(process.argv.length === 2){
+  void api.showAll()
+}
